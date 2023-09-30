b0ea59c0e45a75a4495a4459a614e5eb4a882296
Shubham Yadav <100077254+Shubham185y@users.noreply.github.com>
2023-10-01 00:52:33 +0530

    fixed rating issue

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,40 +1,54 @@
-import react, {useState, useEffect} from 'react'
-import ReactStars from 'react-stars'
+import React, { useEffect, useState } from "react";
+import { Audio, ThreeDots } from "react-loader-spinner";
+import ReactStars from "react-stars";
 import {getDocs} from 'firebase/firestore'
-import { moviesRef } from '../Firebase/firebase'
-import { Link } from 'react-router-dom'
+import {moviesRef} from '../firebase/firebase'
+import { Link } from "react-router-dom";
+
 const Cards = () => {
-    const [loading, setLoading] = useState(false);
-    const[data, setData]= useState([])
-    useEffect(()=>{
-        async function getData(){
-            setLoading(true);
-            const _data = await getDocs(moviesRef)
-            _data.forEach((doc) =>{
-                setData((prv)=>[...prv, {...(doc.data()), id:doc.id}])
-            })
-            setLoading(false)
-        }
-        getData();
-    },[])
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
+      const _data = await getDocs(moviesRef);
+      _data.forEach((doc) => {
+        setData((prv) => [...prv, {...(doc.data()), id: doc.id}])
+      })
+      setLoading(false);
+    }
+    getData();
+  },[])
+
   return (
-    <div className='flex flex-wrap justify-center p-3 mt-2'>
-    {data.map((e,i) => {
-        return(
-    <Link to={`/Details/${e.id}`}><div key={i} className='bg-gray-200 font-bold p-4 shadow-xl rounded-lg m-4 hover:-translate-y-3 transition-all duration-500'>
-        <img className='h-60  md:h-72 w-full justify-center' src={e.img}/>
-        <h1 className='pt-3 pl-3'>
-        <h1><span className='prop'></span>{e.tittle}</h1>
-        <h1 className='flex items-center'><span className='prop mr-1'>Rating: </span><ReactStars size={20} edit={false} half={true} value = {e.rating/e.rated} />
-        </h1>
-        <h1><span className='prop'>Year: </span> {e.year}</h1>  
-        </h1>
-    </div>
-    </Link>
+    <div className="flex flex-wrap justify-between px-3 mt-2">
+    {loading ? <div className="w-full flex justify-center items-center h-96"><ThreeDots height={40} color="white" /></div> : 
+      data.map((e, i) => {
+        return (
+          <Link to={`/detail/${e.id}`}><div key={i} className="card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-500">
+            <img className="h-60 md:h-72" src={e.image} />
+            <h1>
+              {e.title}
+            </h1>
+            <h1 className="flex items-center">
+              <span className="text-gray-500 mr-1">Rating:</span>
+              <ReactStars
+                size={20}
+                half={true}
+                value={e.rating/e.rated}
+                edit={false}
+              />
+            </h1>
+            <h1>
+              <span className="text-gray-500">Year:</span> {e.year}
+            </h1>
+          </div></Link>
         );
-    })}
+      })
+    }
     </div>
-  )
-}
+  );
+};
 
 export default Cards;
